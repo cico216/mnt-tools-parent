ec60db37d1714776e31f04a8b5ea67e20c637500
cico216 <cico216@163.com>
2019-03-03 19:30:30 +0800

    项目初始化

---
+++ protocol-tools/src/main/java/com/mnt/protocol/view/MainViewController.java
@@ -0,0 +1,12 @@
+package com.mnt.protocol.view;
+
+import com.mnt.gui.fx.base.BaseController;
+import com.mnt.gui.fx.view.anno.MainView;
+
+/**
+ * 通讯协议工具类
+ */
+@MainView(appName = "通讯协议工具")
+public class MainViewController extends BaseController {
+
+}
